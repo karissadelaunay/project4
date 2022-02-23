e0d8d5afb32a8259f36af33bfeb05b17947f330f
Karissa DeLaunay <karissadelaunay@karissas-air.lan>
2022-02-22 16:12:12 -0800

    cleaned up routes

--- routes/api/post.js
+++ routes/api/post.js
@@ -2,15 +2,12 @@ const express = require('express');
 const router = express.Router();
 const postsCtrl = require('../../controllers/posts');
 const multer = require('multer');
-const upload = multer(); // <- handles multipart/formdata requests(photos)
-// /*---------- Public Routes ----------*/
+const upload = multer(); 
 
-// photo is the key on the formData object in the AddPost component
+
 router.post('/', isAuthenticated, upload.single('photo'), postsCtrl.create);
 router.get('/', postsCtrl.index)
 
-
-/*---------- Protected Routes ----------*/
 function isAuthenticated(req, res, next){
 	if(req.user){
 		next()
